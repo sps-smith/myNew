14479d84f4b387993741c54788da704c5ac9e451
Phil Smith <phil.smith@volarisgroup.com>
2018-03-21 17:04:15 -0400

    EOD 03/21/2018 - Updates after doing mock list

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,4 +1,4 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version, Environment, EnvironmentType } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
@@ -7,10 +7,14 @@ import {
   PropertyPaneDropdown,
   PropertyPaneToggle
 } from "@microsoft/sp-webpart-base";
+
+
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './HelloWorldWebPart.module.scss';
 import * as strings from 'HelloWorldWebPartStrings';
+import ListArray from './MockHttpClient';
+import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
 export interface IHelloWorldWebPartProps {
   description: string;
@@ -21,74 +25,132 @@ export interface IHelloWorldWebPartProps {
   test3: boolean;
 }
 
+export interface ISPLists{
+  value: ISPList[];
+}
+
+export interface ISPList{
+  Title: string;
+  Id: string;
+}
+
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
-  public render(): void {
-    //this.properties.displaymode = this.displayMode.toString();
-    this.domElement.innerHTML = `
-      <div class="${styles.helloWorld}">
-        <div class="${styles.container}">
-          <div class="${styles.row}">
-            <div class="${styles.column}">
-              <span class="${styles.title}">Welcome to SharePoint!</span>
-              <p class="${styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${styles.description}">${escape(this.properties.description)}</p>
-              <p class="${styles.description}">${escape(this.properties.displaymode)}</p>
-              <a href="https://aka.ms/spfx" class="${styles.button}">
-                <span class="${styles.label}">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
-  }
+	public render(): void {
+		//this.properties.displaymode = this.displayMode.toString();
+		this.domElement.innerHTML = `
+		<div class="${styles.helloWorld}">
+			<div class="${styles.container}">
+			<div class="${styles.row}">
+				<div class="${styles.column}">
+				<span class="${styles.title}">Welcome to SharePoint!</span>
+				<p class="${styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
+				<p class="${styles.description}">Loading from ${escape(this.context.pageContext.web.title)}</p>
+				<p class="${styles.description}">${escape(this.properties.description)}</p>
+				<p class="${styles.description}">${escape(this.properties.displaymode)}</p>
+				<a href="https://aka.ms/spfx" class="${styles.button}">
+					<span class="${styles.label}">Learn more</span>
+				</a>
+				</div>
+			</div>
+			<div id="spListContainer" />
+			</div>
+		</div>`;
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
+		this._renderListAsync();
+	}
 
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                }),
-                PropertyPaneTextField('displaymode', {
-                  label: strings.DescriptionFieldLabel
-                }),
-                PropertyPaneTextField('test', {
-                  label: 'Multi-line Text Field',
-                  multiline: true
-                }),
-                PropertyPaneCheckbox('test1', {
-                  text: 'Checkbox'
-                }),
-                PropertyPaneDropdown('test2', {
-                  label: 'Dropdown',
-                  options: [
-                    { key: '1', text: 'One' },
-                    { key: '2', text: 'Two' },
-                    { key: '3', text: 'Three' },
-                    { key: '4', text: 'Four' }
-                  ]}),
-                PropertyPaneToggle('test3', {
-                  label: 'Toggle',
-                  onText: 'On',
-                  offText: 'Off'
-                })
-              ]
-            }
-          ]
-        }
-      ]
-    };
-  }
+	private _getMockListData(): Promise<ISPLists> {
+		return ListArray.get()
+			.then((data: ISPList[]) => {
+			var listData: ISPLists = { value: data };
+			return listData;
+			}) as Promise<ISPLists>;
+	}
+
+	private _getListData(): Promise<ISPLists> {
+		return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+		.then((response: SPHttpClientResponse) => {
+			return response.json();
+		}) as Promise<ISPLists>;
+	}
+
+	private _renderListAsync(): void {
+   // Local environment
+		if (Environment.type === EnvironmentType.Local) {
+			this._getMockListData().then((response) => {
+			this._renderList(response.value);
+			});
+		}
+		else if (Environment.type == EnvironmentType.SharePoint ||
+					Environment.type == EnvironmentType.ClassicSharePoint) {
+			this._getListData()
+			.then((response) => {
+				this._renderList(response.value);
+			});
+		}
+	}
+
+	private _renderList(items: ISPList[]): void {
+		let html: string = '';
+		items.forEach((item: ISPList) => {
+			html += `
+		<ul class="${styles.list}">
+			<li class="${styles.listItem}">
+			<span class="ms-font-l">${item.Title}</span>
+			</li>
+		</ul>`;
+   });
+
+   const listContainer: Element = this.domElement.querySelector('#spListContainer');
+   listContainer.innerHTML = html;
+ }
+	protected get dataVersion(): Version {
+		return Version.parse('1.0');
+	}
+
+	protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+		return {
+			pages: [
+				{
+					header: {
+						description: strings.PropertyPaneDescription
+					},
+					groups: [
+						{
+							groupName: strings.BasicGroupName,
+							groupFields: [
+								PropertyPaneTextField('description', {
+									label: strings.DescriptionFieldLabel
+								}),
+								PropertyPaneTextField('displaymode', {
+									label: strings.DescriptionFieldLabel
+								}),
+								PropertyPaneTextField('test', {
+									label: 'Multi-line Text Field',
+									multiline: true
+								}),
+								PropertyPaneCheckbox('test1', {
+									text: 'Checkbox'
+								}),
+								PropertyPaneDropdown('test2', {
+									label: 'Dropdown',
+								options: [
+									{ key: '1', text: 'One' },
+									{ key: '2', text: 'Two' },
+									{ key: '3', text: 'Three' },
+									{ key: '4', text: 'Four' }
+								]}),
+								PropertyPaneToggle('test3', {
+									label: 'Toggle',
+									onText: 'On',
+									offText: 'Off'
+								})
+							]
+					}
+				]
+			}
+		]
+	};
+	}
 }
